b4660afd5300e24e319e414f8c75f4d379effcca
Ha.Minh <minhhh@minhhuyha.info>
2016-11-29 13:31:44 +0700

    Initial commit

---
+++ Assets/Editor/ShurikenScaler.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class ShurikenScaler : EditorWindow
+{
+    [MenuItem ("Window/ShurikenScaler")]
+    public static void OpenWindow ()
+    {
+        GetWindow<ShurikenScaler> (false, "Shuriken Particle Scaler", true);
+    }
+
+    ParticleSystem[] particleSystems = null;
+
+    float oldScale = 1.0f;
+    float currentScale = 1.0f;
+    int currentInstanceId = 0;
+    static Dictionary<int, float> currentScales = new Dictionary<int, float> ();
+
+    void OnEnable ()
+    {
+//        Debug.Log (GetType ().Name + "::OnEnabled");
+        Init ();
+    }
+
+    void OnDisable ()
+    {
+//        Debug.Log (GetType ().Name + "::OnDisable");
+    }
+
+    void OnSelectionChange ()
+    {
+//        Debug.Log (GetType ().Name + "::OnSelectionChange");
+        Init ();
+        Repaint ();
+    }
+
+    void Init ()
+    {
+        particleSystems = null;
+
+        if (Selection.activeGameObject == null)
+            return;
+
+        particleSystems = Selection.activeGameObject.GetComponentsInChildren <ParticleSystem> ();
+
+        currentInstanceId = Selection.activeGameObject.GetInstanceID ();
+        if (!currentScales.ContainsKey (currentInstanceId)) {
+            currentScales [currentInstanceId] = 1.0f;
+        }
+        currentScale = currentScales [currentInstanceId];
+    }
+
+    void OnGUI ()
+    {
+        if (particleSystems == null || particleSystems.Length == 0) {
+            EditorGUILayout.HelpBox ("Please select an object with ParticleSystem components in itself or its children", MessageType.Error);
+            return;
+        }
+            
+
+        EditorGUILayout.Space ();
+
+        EditorGUILayout.BeginHorizontal ();
+
+        EditorGUI.BeginChangeCheck ();
+        oldScale = currentScale;
+        currentScale = EditorGUILayout.Slider (currentScale, 0.1f, 100);
+
+        if (EditorGUI.EndChangeCheck ()) {
+            DoScale ();
+        }
+
+        if (GUILayout.Button ("Play")) {
+            foreach (var ps in particleSystems) {
+                ps.Play (true);
+            }
+        }
+        if (GUILayout.Button ("Pause")) {
+            foreach (var ps in particleSystems) {
+                ps.Pause (true);
+            }
+        }
+        if (GUILayout.Button ("Stop")) {
+            foreach (var ps in particleSystems) {
+                ps.Stop (true);
+            }
+        }
+        EditorGUILayout.EndHorizontal ();
+
+        if (GUILayout.Button ("Reset")) {
+            currentScale = 1.0f;
+            DoScale ();
+        }
+
+        if (GUI.changed) {
+            SceneView.RepaintAll ();
+        }
+    }
+
+    void DoScale ()
+    {
+        currentScales [currentInstanceId] = currentScale;
+
+        var go = Selection.activeGameObject;
+        foreach (var ps in particleSystems) {
+            ps.Stop ();
+            ScaleParticleSystem (go, ps);
+            ps.Play ();
+        }
+    }
+
+    void ScaleParticleSystem (GameObject go, ParticleSystem ps)
+    {
+        var multiplier = currentScale / oldScale;
+
+        if (go != ps.gameObject) {
+            ps.gameObject.transform.localPosition *= multiplier;
+        }
+
+        ps.startSize *= multiplier;
+        ps.gravityModifier *= multiplier;
+        ps.startSpeed *= multiplier;
+
+        SerializedObject so = new SerializedObject (ps);
+
+        so.FindProperty ("VelocityModule.x.scalar").floatValue *= multiplier;
+        so.FindProperty ("VelocityModule.y.scalar").floatValue *= multiplier;
+        so.FindProperty ("VelocityModule.z.scalar").floatValue *= multiplier;
+        ScaleCurve (so.FindProperty ("VelocityModule.x.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("VelocityModule.x.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("VelocityModule.y.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("VelocityModule.y.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("VelocityModule.z.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("VelocityModule.z.maxCurve").animationCurveValue, multiplier);
+        so.FindProperty ("ClampVelocityModule.magnitude.scalar").floatValue *= multiplier;
+        so.FindProperty ("ClampVelocityModule.x.scalar").floatValue *= multiplier;
+        so.FindProperty ("ClampVelocityModule.y.scalar").floatValue *= multiplier;
+        so.FindProperty ("ClampVelocityModule.z.scalar").floatValue *= multiplier;
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.x.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.x.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.y.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.y.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.z.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.z.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.magnitude.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ClampVelocityModule.magnitude.maxCurve").animationCurveValue, multiplier);
+        so.FindProperty ("ForceModule.x.scalar").floatValue *= multiplier;
+        so.FindProperty ("ForceModule.y.scalar").floatValue *= multiplier;
+        so.FindProperty ("ForceModule.z.scalar").floatValue *= multiplier;
+        ScaleCurve (so.FindProperty ("ForceModule.x.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ForceModule.x.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ForceModule.y.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ForceModule.y.maxCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ForceModule.z.minCurve").animationCurveValue, multiplier);
+        ScaleCurve (so.FindProperty ("ForceModule.z.maxCurve").animationCurveValue, multiplier);
+        so.FindProperty ("ColorBySpeedModule.range").vector2Value *= multiplier;
+        so.FindProperty ("SizeBySpeedModule.range").vector2Value *= multiplier;
+        so.FindProperty ("RotationBySpeedModule.range").vector2Value *= multiplier;
+
+        so.ApplyModifiedProperties ();
+    }
+
+    void ScaleCurve (AnimationCurve curve, float multiplier)
+    {
+        for (int i = 0; i < curve.keys.Length; i++) {
+            curve.keys [i].value *= multiplier;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # unity-particle-scaler
+
+One of the common problems when working with Unity Shuriken system is that it does not support scaling particles out of the box. When you change the scale of the parent, the children particles do not scale. Even if you somehow get them to scale using scaling mode `Hierarchy`, the end result still does not look good because the velocity of the particles does not scale.
+
+This repo contains an Editor script called `ShurikenScaler` which can help you scale Shuriken particles easily. It will provide an Editor Window that can be accessed via `Window > ShurikenScaler`.
+
+![example 1](/images/example1.png)
+
+You can use this Editor Window when you have an GameObject with a `ParticleSystem` component in itself or one of its children. First, select the GameObject in the scene. Then you can change the scale from 0.1 to 100 times the original.
+
+You can click `Reset` to set the scale back to 1. You can also select other GameObject and it will remember the scale of the previous ones. Internally, it uses a static array to store the scale by InstanceId.
+
+Does not support run-time scaling.
+
+## Change History
+
+**Version 1.0.0**
+
+* Initial commit
